6d4b733dc5354f54b6db63ab33baf8ddff9b53ca
OrigamiYoda <52184332+OrigamiYoda@users.noreply.github.com>
2023-03-22 23:07:06 -0400

    Needed driveTrainMantis merge

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -31,9 +31,11 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -198,16 +200,5 @@ public class RobotContainer {
     return swerveSubsystem;
   }
 
-  // public static ArmSubsystem getArmSubsystem(){
-  //   return armSubsystem;
-  // }
-  
-  // public static GripperSubsystem getGripperSubsystem(){
-  //   return gripperSubsystem;
-  // }
-
-  // public static BucketSubsystem getBuckSubsystem(){
-  //   return bucketSubsystem;
-  // }
 
 }
