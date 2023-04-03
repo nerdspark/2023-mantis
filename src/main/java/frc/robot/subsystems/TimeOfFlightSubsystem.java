21ed7a9993629b837d40a9ec7f79122534009239
jayden <jayden@viewsource.me>
2023-04-02 20:43:47 -0400

    revert tof changes

--- src/main/java/frc/robot/subsystems/TimeOfFlightSubsystem.java
+++ src/main/java/frc/robot/subsystems/TimeOfFlightSubsystem.java
@@ -6,47 +6,24 @@ package frc.robot.subsystems;
 
 import com.playingwithfusion.TimeOfFlight;
 import com.playingwithfusion.TimeOfFlight.RangingMode;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotContainer;
 
 public class TimeOfFlightSubsystem extends SubsystemBase {
-    com.playingwithfusion.TimeOfFlight timeOfFlight;
+    TimeOfFlight timeOfFlight;
     //    MedianFilter filter = new MedianFilter(5);
 
-    //    double range;
-
-    /** Creates a new ExampleSubsystem. */
     public TimeOfFlightSubsystem() {
         this.timeOfFlight = new TimeOfFlight(15);
-        timeOfFlight.setRangingMode(RangingMode.Short, 36);
+        timeOfFlight.setRangingMode(RangingMode.Short, 24);
     }
 
     @Override
     public void periodic() {
-        // SmartDashboard.putNumber("sensor position", sensor1.getAbsolutePosition());
-        //        range = filter.calculate(timeOfFlight.getRange());
-        SmartDashboard.putNumber("TOF Range", this.getRange());
-        SmartDashboard.putBoolean(
-                "cube pickup tof",
-                (RobotContainer.getArmSubsystem().getArmPositionState() == ArmSubsystem.ArmPosition.CUBE_PICKUP)
-                        && (this.getRange() < 75)
-                        && (this.getRange() > 30));
-    }
-
-    // public void printSensor() {
-    // SmartDashboard.putNumber("sensor position", sensor1.getAbsolutePosition());
-    // }
-    @Override
-    public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
+        //        SmartDashboard.putNumber("TOF Reading with mean", filter.calculate(timeOfFlight.getRange()));
+        //        SmartDashboard.putNumber("TOF Reading without mean", timeOfFlight.getRange());
     }
 
     public double getRange() {
         return timeOfFlight.getRange();
-    }
-
-    public void closeTimeOfFlight() {
-        timeOfFlight.close();
     }
 }
